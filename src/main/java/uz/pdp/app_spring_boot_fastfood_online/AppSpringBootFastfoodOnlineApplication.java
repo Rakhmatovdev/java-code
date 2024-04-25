236bc628260fd0d11f8c3349d017e43ac1ed5e83
Nurmukhammad Sobirov <nurmukhammadsobirov@Nurmukhammads-MacBook-Air.local>
2024-04-25 12:44:17 +0500

    first commit

---
+++ src/main/java/uz/pdp/app_spring_boot_fastfood_online/AppSpringBootFastfoodOnlineApplication.java
@@ -0,0 +1,13 @@
+package uz.pdp.app_spring_boot_fastfood_online;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppSpringBootFastfoodOnlineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppSpringBootFastfoodOnlineApplication.class, args);
+	}
+
+}
---
+++ src/test/java/uz/pdp/app_spring_boot_fastfood_online/AppSpringBootFastfoodOnlineApplicationTests.java
@@ -0,0 +1,13 @@
+package uz.pdp.app_spring_boot_fastfood_online;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppSpringBootFastfoodOnlineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
